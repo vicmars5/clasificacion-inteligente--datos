604b402f12cb6f0cb1e093590d8960bba73ad3f7
Victor Diaz <mars.vic5@gmail.com>
2018-09-23 16:22:53 -0500

    docs

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Dependecies:
 - [commandr](https://github.com/tellapart/commandr)
+
+## How to run
+```
+pip install -r requirements.txt
+python app.py --help
+```
--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from commandr import command, Run
 
 @command('add')
 def addition(argA, argB):
+    """ <a> <b>
+    Add one number to another
+
+    Arguments:
+        <item> First number
+        <item> Second number
+    """
     a = float(argA)
     b = float(argB)
     total = a + b
@@ -9,6 +16,13 @@ def addition(argA, argB):
 
 @command('substract')
 def substract(argA, argB):
+    """ <a> <b>
+    Substract "b" from "a"
+
+    Arguments:
+        <item> First number
+        <item> Second number
+    """
     a = float(argA)
     b = float(argB)
     result = a - b
@@ -16,6 +30,13 @@ def substract(argA, argB):
 
 @command('times')
 def time(argA, argB):
+    """ <a> <b>
+    Multiply "b" and "a"
+
+    Arguments:
+        <item> First number
+        <item> Second number
+    """
     a = float(argA)
     b = float(argB)
     result = a * b
@@ -23,6 +44,13 @@ def time(argA, argB):
 
 @command('divide')
 def divide(argA, argB):
+    """ <a> <b>
+    Divide "a" / "b"
+
+    Arguments:
+        <item> First number
+        <item> Second number
+    """
     a = float(argA)
     b = float(argB)
     result = a / b
@@ -30,6 +58,13 @@ def divide(argA, argB):
 
 @command('pow')
 def pow(argA, argB):
+    """ <a> <b>
+    Power - "a" **"b"
+
+    Arguments:
+        <item> First number
+        <item> Second number
+    """
     a = float(argA)
     b = float(argB)
     result = a ** b
