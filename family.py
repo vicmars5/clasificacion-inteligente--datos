3a6ddebabb4006e993953edc76cdcc229b35d1a5
Victor Diaz <mars.vic5@gmail.com>
2018-09-25 18:31:04 -0500

    add and delete family members from list, use sort and reverse methods on places

---
+++ family.py
@@ -0,0 +1,32 @@
+family = []
+
+print("familia", str(family))
+
+family.append("Agustin")
+print("familia", str(family))
+
+
+family.append("Maria")
+print("familia", str(family))
+
+
+family.append("Max")
+print("familia", str(family))
+
+family.append("Alex")
+print("familia", str(family))
+
+
+family.remove("Alex")
+print("familia", str(family))
+
+
+family.remove("Agustin")
+print("familia", str(family))
+
+family.remove("Maria")
+print("familia", str(family))
+
+family.remove("Max")
+print("familia", str(family))
+
---
+++ places.py
@@ -0,0 +1,20 @@
+places = [
+  "San Francisco",
+  "Tokio",
+  "Cuba",
+  "Hong Kong",
+  "Paises Bajos"
+]
+
+print("places", places)
+print("sorted places", sorted(places))
+print("original places", places)
+places.reverse()
+print("reversed places", places)
+places.reverse()
+print("reversed again places", places)
+places.sort()
+print("sort places", places)
+places.sort(reverse=True)
+print("sort again places", places)
+
