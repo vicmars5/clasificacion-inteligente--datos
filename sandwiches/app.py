ba4d40ad549933fc8764ff28c0697fe1a53bdd01
Victor Diaz <mars.vic5@gmail.com>
2018-11-11 22:56:28 -0600

    sandwiches

---
+++ sandwiches/app.py
@@ -0,0 +1,8 @@
+def ask_sandwich(*topics):
+    print("Topics: ")
+    for topic in topics:
+        print(topic)
+    print("")
+
+ask_sandwich("Ham", "Cream", "Mustard")
+ask_sandwich("Ham", "Cream", "Mustard", "Lettuce")
